f71c7744681e6955b57252d0f087a74453c2fd69
# <zwan0549@student.monash.edu>
2025-08-13 11:03:24 +0800

    Add base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FIT5032_2025_Wang_Zhengyao_35522968/',
   plugins: [
     vue(),
     vueDevTools(),
